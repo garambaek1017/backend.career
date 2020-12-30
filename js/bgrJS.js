de88bb5110164fce855d2d4543ee580054b85d52
garambaek <bgr1017.kr@gmail.com>
2020-12-30 23:04:02 +0900

    html 변경 및 새단장 나중에 다시 정리할 예정

---
+++ js/bgrJS.js
@@ -0,0 +1,9 @@
+﻿ 
+
+
+$(document).ready(function () {
+
+    $("#navMenu").load("navMenu.html");  // 원하는 파일 경로를 삽입하면 된다
+
+    // $("#footers").load("footers.html");  // 추가 인클루드를 원할 경우 이런식으로 추가하면 된다
+});
